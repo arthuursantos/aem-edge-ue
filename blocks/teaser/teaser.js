6149b1bf9d2a398ef029a4152d426ae8bd5acb37
Arthur Santos <arthur.santos@vilt.local>
2025-09-02 17:45:59 -0300

    feat(teaser) add side-by-side block options

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,3 +1,34 @@
+/**
+ * Block options are applied as classes to the block's DOM element
+ * alongside the `block` and `<block-name>` classes.
+ *
+ * @param {HTMLElement} block represents the block's DOM element/tree
+ */
+function getOptions(block) {
+  // Get the block's classes, excluding 'block' and 'teaser'.
+  return [...block.classList].filter((c) => !['block', 'teaser'].includes(c));
+}
+
+/**
+ * Entry point to the block's JavaScript.
+ * Must be exported as default and accept a block's DOM element.
+ * This function is called by the project's style.js, passing the block's element.
+ *
+ * @param {HTMLElement} block represents the block's DOM element/tree
+ */
 export default function decorate(block) {
-  block.querySelector('picture').classList.add('image-wrapper');
+  /* Common treatments for all options */
+  block.querySelector(':scope > div:last-child').classList.add('content');
+  block.querySelector('h1,h2,h3,h4,h5,h6').classList.add('title');
+  block.querySelector('img').classList.add('image');
+
+  /* Conditional treatments for specific options */
+  if (['side-by-side-right', 'side-by-side-left'].some((v) => getOptions(block).includes(v))) {
+    /* For side-by-side teaser, add the image-wrapper to a higher-level div to support CSS */
+    block.querySelector(':scope > div:first-child').classList.add('image-wrapper');
+    block.closest('.teaser-container').classList.add('container-side-by-side');
+  } else if (!getOptions(block)) {
+    /* For the default option, add the image-wrapper to the picture element to support CSS */
+    block.querySelector('picture').classList.add('image-wrapper');
+  }
 }
