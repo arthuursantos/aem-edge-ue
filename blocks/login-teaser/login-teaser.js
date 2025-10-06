569ad06903d3cdfda079fb3787239159c1bdfc65
Arthur Santos <arthur.santos@vilt.local>
2025-10-06 17:49:11 -0300

    debug(spring): login-teaser

--- blocks/login-teaser/login-teaser.js
+++ blocks/login-teaser/login-teaser.js
@@ -1,3 +1,35 @@
+import Api from '../../src/utils/api.js';
+
 export default function decorate(block) {
   const [pictureDiv, titleDiv, textDiv] = block.children;
+  pictureDiv.classList.add('login-teaser__picture');
+  titleDiv.classList.add('login-teaser__title');
+  textDiv.classList.add('login-teaser__text');
+
+  const sideDiv = document.createElement('div');
+  sideDiv.classList.add('login-teaser__side');
+  sideDiv.append(titleDiv, textDiv);
+  block.append(sideDiv);
+
+  const formEl = document.createElement('form');
+  formEl.classList.add('login-teaser__form');
+  const emailInput = document.createElement('input');
+  emailInput.setAttribute('type', 'email');
+  emailInput.setAttribute('placeholder', 'Email');
+  const passwordInput = document.createElement('input');
+  passwordInput.setAttribute('type', 'password');
+  passwordInput.setAttribute('placeholder', 'Senha');
+
+  const submitBtn = document.createElement('input');
+  submitBtn.setAttribute('type', 'submit');
+  formEl.append(emailInput, passwordInput, submitBtn);
+  block.append(formEl);
+
+  submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    Api.login()
+      .then((r) => {
+        block.append(r.body);
+      });
+  });
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,9 @@
 export default class Api {
   static hostname = 'http://localhost:8080';
 
-  static fetch = async (endpoint) => {
+  static fetch = async (endpoint, req) => {
     try {
-      const res = await fetch(`${Api.hostname}/${endpoint}`);
+      const res = await fetch(`${Api.hostname}${endpoint}`, req);
       if (!res.ok) throw new Error(`HTTP ${res.status}: ${await res.text()}`);
       return res;
     } catch {
@@ -11,8 +11,8 @@ export default class Api {
     }
   };
 
-  static login = async (endpoint) => {
-    const res = fetch(endpoint, {
+  static login = async () => {
+    const res = await this.fetch('/auth/login', {
       headers: {
         'Content-Type': 'application/json',
       },
